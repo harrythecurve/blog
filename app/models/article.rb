576b6b71ba6098667cbb49c5af9e6db30052209b
Harry Wijnschenk <harry.wijnschenk@thecurve.io>
2023-10-02 20:28:50 +0100

    User created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 10, maximum: 100 }
   validates :description, presence: true, length: { minimum: 20, maximum: 255 }
 end
