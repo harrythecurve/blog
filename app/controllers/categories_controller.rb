1efaf53c9fc1a08bbd8812ada9bf0e2229a0f548
harrythecurve <134274074+harrythecurve@users.noreply.github.com>
2023-10-25 19:17:54 +0100

    Cateogires associated with articles (#9)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :load_category, only: %i[show]
-  before_action except: %i[index show] do
+  before_action only: %i[new create] do
     require_admin(categories_path)
   end
 
@@ -8,7 +8,9 @@ class CategoriesController < ApplicationController
     @categories = Category.paginate(page: params[:page], per_page: 9)
   end
 
-  def show; end
+  def show
+    @articles = @category.articles.paginate(page: params[:page], per_page: 9)
+  end
 
   def new
     @category = Category.new
--- app/models/category.rb
+++ app/models/category.rb
@@ -8,5 +8,8 @@
 #  updated_at :datetime         not null
 #
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
 end
